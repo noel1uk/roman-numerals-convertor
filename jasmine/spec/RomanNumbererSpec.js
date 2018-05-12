63011fad5d4d6095de28f54e5d7ecee167e3286b
Noel Vock <noel1uk@gmail.com>
2018-05-12 10:55:00 +0100

    test: ensure it returns III when given 3

--- jasmine/spec/RomanNumbererSpec.js
+++ jasmine/spec/RomanNumbererSpec.js
@@ -4,5 +4,8 @@ describe('Romanizer', function() {
     it('returns I when given 1', function() {
       expect(romanize.convert(1)).toEqual('I')
     });
+    it('returns III when given 3', function() {
+      expect(romanize.convert(3)).toEqual('III')
+    });
   });
 });
