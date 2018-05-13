718eeea2977f34b8082c5c09a414bd0fd7cc9806
Noel Vock <noel1uk@gmail.com>
2018-05-13 14:11:43 +0100

    feat: categorize returns a category

--- jasmine/src/RomanNumberer.js
+++ jasmine/src/RomanNumberer.js
@@ -4,9 +4,21 @@ class RomanNumberer {
   }
   convert(number) {
     let answer = '';
-    for ( let i = 0; i < number; i++ ) {
-      answer += 'I'
+    let category = this.categorize(number);
+    switch (category) {
+      case 'first':
+        for ( let i = 0; i < number; i++ ) {
+          answer += 'I';
+        }
+      break;
+      default:
+        answer += 'IV';
     }
     return answer;
+  }
+  categorize(number) {
+    if ( number < 4) {
+      return 'first';
+    }
   }
 }
