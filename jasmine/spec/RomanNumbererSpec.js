4d36a4e011e8091504075ccce678c0579182e89b
Noel Vock <noel1uk@gmail.com>
2018-05-13 14:23:16 +0100

    test: add expectation for the number 5

--- jasmine/spec/RomanNumbererSpec.js
+++ jasmine/spec/RomanNumbererSpec.js
@@ -2,18 +2,21 @@ describe('Romanizer', function() {
   let romanize = new RomanNumberer();
   describe('.convert', function() {
     it('returns I when given 1', function() {
-      expect(romanize.convert(1)).toEqual('I')
+      expect(romanize.convert(1)).toEqual('I');
     });
     it('returns III when given 3', function() {
-      expect(romanize.convert(3)).toEqual('III')
+      expect(romanize.convert(3)).toEqual('III');
     });
     it('returns IV when given 4', function() {
-      expect(romanize.convert(4)).toEqual('IV')
+      expect(romanize.convert(4)).toEqual('IV');
+    });
+    it ('returns 5 when given 5', function() {
+      expect(romanize.convert(5)).toEqual(V);
     });
   });
   describe('.categorize', function() {
     it('when given 3 it returns the string `first`', function() {
-      expect(romanize.categorize(3)).toEqual('first')
+      expect(romanize.categorize(3)).toEqual('first');
     });
   });
 });
