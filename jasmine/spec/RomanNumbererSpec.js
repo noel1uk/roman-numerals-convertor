40d73e52d2117f6a4412df0f18a816f0caf4ab13
Noel Vock <noel1uk@gmail.com>
2018-05-13 12:44:50 +0100

    test: ensure it gives correct category when given number 3

--- jasmine/spec/RomanNumbererSpec.js
+++ jasmine/spec/RomanNumbererSpec.js
@@ -7,5 +7,13 @@ describe('Romanizer', function() {
     it('returns III when given 3', function() {
       expect(romanize.convert(3)).toEqual('III')
     });
+    it('returns IV when given 4', function() {
+      expect(romanize.convert(4)).toEqual('IV')
+    });
+  });
+  describe('.categorize', function() {
+    it('when given 3 it returns the string `first`', function() {
+
+    });
   });
 });
